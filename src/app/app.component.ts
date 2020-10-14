fd0dd913815a259824571b4ff03d2c49bf7a860f
ariel <estagiosenaiempresa3@ms.senai.br>
2020-10-14 11:37:57 -0400

    Criadno uma pagina de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Sair', component: 'HomePage' },
+      { title: 'Paciente', component: 'PacientePage' },
+      { title: 'Profile', component: 'ProfilePage' }
     ];
 
   }
